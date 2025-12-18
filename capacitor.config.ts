5dd363517dda159ab6e68cee3e183a221836c089
Freddy Reyes <freddy.reyes729@gmail.com>
2025-12-17 22:09:09 -0500

    Adds Firebase hosting integration and splash screen config
    Integrates Firebase Hosting with automated GitHub Actions for deployments on merges and pull requests.
    Configures initial Firebase hosting settings and updates the ignore list for Android build resources.
    Enhances splash screen behavior and appearance, and adds relevant Capacitor and Firebase dependencies to support these features.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,18 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'CatAppPragma',
-  webDir: 'www/browser'
+  webDir: 'www/browser',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000,     // ⏱ tiempo visible (ms)
+      launchAutoHide: true,         // ✅ mostrar automáticamente
+      backgroundColor: '#ffffff',   // ⚠️ importante
+      androidScaleType: 'CENTER_CROP',
+      showSpinner: false,
+      splashFullScreen: true,
+      splashImmersive: true
+    }
+  }
 };
 
 export default config;
